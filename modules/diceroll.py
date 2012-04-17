af58a0492f52d8275d1ccce9aefceee3d53e3060
Space A. Splode <spacexplosion@gmail.com>
2012-04-17 16:41:21 -0400

    roll some RPG dice

---
+++ modules/diceroll.py
@@ -0,0 +1,24 @@
+import irclib
+import re
+import random
+import modules
+
+class IRCModule(modules.CommandMod):
+
+    pattern = re.compile("^!roll (\d+)d(\d+)([+-]\d+)?", 
+                         re.UNICODE | re.IGNORECASE)
+
+    def on_command(self, connection, commander, replyto, groups):
+        n = s = m = sum = 0
+        try:
+            n = int(groups[0])
+            s = int(groups[1])
+            if groups[2] is not None:
+                m = int(groups[2])
+        except ValueError:
+            return
+        for i in range(n):
+            sum += random.randint(1, s)
+        sum += m
+        connection.privmsg(replyto,"%s rolls %d" % \
+                               (irclib.nm_to_n(commander),  sum))
