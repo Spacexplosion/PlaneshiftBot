74ada29df005fe66dc3cd34d6a069000d007aa29
Space A. Splode <spacexplosion@gmail.com>
2014-03-22 11:58:54 -0400

    Debian init dependency support

--- botinit.sh
+++ botinit.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
+#
 # A simple init script to keep the bot running
+#
+### BEGIN INIT INFO
+# Provides:          planeshiftbot
+# Required-Start:    networking cron
+# Required-Stop:     networking
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: Start/Stop a PlaneshiftBot
+# Description:       Starts a PlaneshiftBot with the user specified by the 
+#                    symlink to this script.
+### END INIT INFO
+
 botuser="${0##*/}"
 eval pidfile=~$botuser/bot.pid
 if [[ -f $pidfile ]]; then
