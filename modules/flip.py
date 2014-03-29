4b197e484df4bfe9c76d4aabbeb82c5a316c38cb
Space A. Splode <spacexplosion@gmail.com>
2014-03-28 23:02:23 -0400

    flip text module

---
+++ modules/flip.py
@@ -0,0 +1,21 @@
+import upsidedown
+import re
+import irc
+import logging
+import modules
+import config
+
+class IRCModule(modules.CommandMod):
+    """Flip text upside down
+
+    Requires upsidedown package. (https://pypi.python.org/pypi/upsidedown)"""
+
+    patternstr = "flip\s+(.+)"
+    help = "Gets basic info on this bot"
+
+    def __init__(self):
+        self.log = logging.getLogger("irc.flip")
+
+    def on_command(self, connection, commander, replyto, groups):
+        response = u"(\u256F\u00B0\u25A1\u00B0)\u256F " + upsidedown.transform(groups[0])
+        connection.privmsg(replyto, response)
